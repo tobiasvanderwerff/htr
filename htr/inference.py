18d574c5f791681bc994b2884538d14389f75956
Tobias van der Werff <33268192+tobiasvanderwerff@users.noreply.github.com>
2022-03-07 12:18:01 +0100

    Update `inference.py`, now writes predictions to file

--- htr/inference.py
+++ htr/inference.py
@@ -1,7 +1,17 @@
+"""
+Run inference on a model and write predictions to a file. NOTE: currently only
+works for FPHTR.
+
+Also worth noting is that because the images are not batched but processed
+individually, no padding is applied to them, in contrast to the setup during training.
+"""
+
+
 import argparse
 from pathlib import Path
-from typing import Tuple, Union, List
+from typing import Tuple, Union, List, Optional, Callable
 
+from htr.data import IAMDataset
 import torch
 import torch.nn.functional as F
 import numpy as np
@@ -13,92 +23,73 @@ from htr.transforms import IAMImageTransforms
 from htr.util import LabelEncoder
 
 
-IMG_SCALES_GRID_SEARCH = [0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
-
-
 def run_model(
     model_path: Path,
-    imgs: Union[torch.Tensor, List[torch.Tensor]],
+    imgs: Union[Path, List[Path]],
     label_encoder: LabelEncoder,
-    print_perplexities: bool = False,
-) -> str:
+    img_transform: Optional[Callable] = None,
+) -> None:
+    """Run inference and write all predictions to a file."""
+    all_preds, pred_strs = [], []
+
     # Load model.
     model = LitFullPageHTREncoderDecoder.load_from_checkpoint(
         str(model_path),
         label_encoder=label_encoder,
     )
 
-    # Make prediction.
+    # Make prediction(s).
     model.eval()
-    if isinstance(imgs, list):  # grid search on image resolutions
-        # Use perplexity as a metric to determine the best image resolution, where we
-        # define perplexity as the exponential of the cross-entropy.
-        perplexities, predictions = [], []
-        for im in imgs:
-            with torch.inference_mode():
-                logits, preds, _ = model(im.unsqueeze(0))
-                pred_probs, _ = F.softmax(logits.squeeze(), -1).max(-1)
-                prp = torch.exp(torch.sum(-torch.log(pred_probs)) / pred_probs.size(0))
-
-                perplexities.append(prp.item())
-                predictions.append(preds.squeeze())
-        best = perplexities.index(min(perplexities))
-        preds = predictions[best]
-        if print_perplexities:
-            print(f"Perplexities: {perplexities}")
-            print(
-                f"Best resolution scaling factor: {IMG_SCALES_GRID_SEARCH[best]}",
-                end="\n\n",
-            )
-    else:  # single image
-        img = imgs
+    if isinstance(imgs, Path):
+        img_path = imgs
+        img = prepare_image(img_path, img_transform)
         with torch.inference_mode():
             _, preds, _ = model(img.unsqueeze(0))
-            preds.squeeze_()
+            preds = preds.squeeze().numpy()
+        all_preds.append((str(img_path), preds))
+    else:  # List[Path]
+        for img_path in imgs:
+            img = prepare_image(img_path, img_transform)
+            with torch.inference_mode():
+                _, preds, _ = model(img.unsqueeze(0))
+                preds = preds.squeeze().numpy()
+            all_preds.append((str(img_path), preds))
 
-    # Decode the prediction.
-    sampled_ids = preds.numpy()[1:]
-    if sampled_ids[-1] == label_encoder.transform(["<EOS>"])[0]:
-        sampled_ids = sampled_ids[:-1]
-    pred_str = "".join((label_encoder.inverse_transform(sampled_ids)))
+    # Decode the prediction(s).
+    for img_path, sampled_ids in all_preds:
+        if sampled_ids[0] == label_encoder.transform(["<SOS>"])[0]:
+            sampled_ids = sampled_ids[1:]
+        if sampled_ids[-1] == label_encoder.transform(["<EOS>"])[0]:
+            sampled_ids = sampled_ids[:-1]
+        pred_strs.append(
+            (str(img_path), "".join((label_encoder.inverse_transform(sampled_ids))))
+        )
 
-    return pred_str
+    # Write the predictions to a file.
+    pred_strs.sort(key=lambda x: x[0])
+    to_write = "\n".join("\t".join([pth, pred]) for pth, pred in pred_strs)
+    Path("model_predictions.txt").write_text(to_write)
 
 
-def prepare_data(
-    img_path: Path,
-    model_path,
-    data_format: str = "word",
-    search_resolution: bool = False,
-) -> Tuple[torch.Tensor, LabelEncoder]:
-    # Load the image.
+def prepare_image(img_path: Path, transform: Optional[Callable] = None):
     img = cv.imread(str(img_path), cv.IMREAD_GRAYSCALE)
-
     assert isinstance(img, np.ndarray), (
         f"Error: image at path {img_path} is not properly loaded. "
         f"Is there something wrong with this image?"
     )
+    if transform is not None:
+        img = torch.from_numpy(transform(image=img)["image"])
+    return img
 
-    # Load the label encoder for the trained model.
+
+def load_label_encoder(model_path: Path):
     le_dir = model_path.parent.parent
     le_path = (
         le_dir / "label_encoder.pkl"
         if (le_dir / "label_encoder.pkl").is_file()
         else le_dir / "label_encoding.txt"
     )
-    label_enc = LabelEncoder().read_encoding(le_path)
-
-    # Apply image transforms.
-    imgs = []
-    if search_resolution:
-        for scale in IMG_SCALES_GRID_SEARCH:
-            trnsf = IAMImageTransforms(img.shape, data_format, scale=scale).test_trnsf
-            imgs.append(torch.from_numpy(trnsf(image=img)["image"]))
-    else:
-        trnsf = IAMImageTransforms(img.shape, data_format).test_trnsf
-        imgs = torch.from_numpy(trnsf(image=img)["image"])
-
-    return imgs, label_enc
+    return LabelEncoder().read_encoding(le_path)
 
 
 if __name__ == "__main__":
@@ -106,28 +97,36 @@ if __name__ == "__main__":
 
     # fmt: off
     parser.add_argument("--img_path", type=Path, required=True,
-                        help="Path to an image for which a prediction should be made.")
+                        help="Path to an image for which a prediction should be made. "
+                             "If a directory is specified, run inference on all "
+                             "images in that directory.")
     parser.add_argument("--model_path", type=Path, required=True,
                         help="Path to a trained model checkpoint, which will be loaded "
                              "for inference.")
     parser.add_argument("--data_format", type=str, choices=["form", "line", "word"],
-                        required=True, help=("Data format of the image. Is it a full "
-                                             "page, a line, or a word?"))
-    parser.add_argument("--search_resolution", action="store_true", default=False,
-                        help=("Set this flag to do a small grid search to find the best "
-                              "resolution for the input image, based on model "
-                              "perplexity. Try this out if the model predictions are not "
-                              "satisfactory."))
+                        required=True, help=("Data format of the image(s). Is it a "
+                                             "full page, a line, or a word?"))
     args = parser.parse_args()
     # fmt: on
 
     img_path, model_path = args.img_path, args.model_path
     assert model_path.is_file(), f"{model_path} does not point to a file."
-    assert img_path.is_file(), f"Image path {img_path} does not point to a file."
+    assert (
+        img_path.is_file() or img_path.is_symlink() or img_path.is_dir()
+    ), f"Image path {img_path} does not point to a file or directory."
 
-    imgs, label_encoder = prepare_data(
-        img_path, model_path, args.data_format, args.search_resolution
-    )
-    pred_str = run_model(model_path, imgs, label_encoder, print_perplexities=False)
+    # Load the label encoder for the trained model.
+    label_encoder = load_label_encoder(model_path)
 
-    print(pred_str)
+    # Load image transform.
+    img_transform = IAMImageTransforms(
+        (0, 0), args.data_format, (IAMDataset.MEAN, IAMDataset.STD)
+    ).test_trnsf
+
+    imgs = img_path
+    if img_path.is_dir():
+        # Search for all images in a directory.
+        imgs = list(img_path.rglob("*.png")) + list(img_path.rglob("*.jpg"))
+
+    run_model(model_path, imgs, label_encoder, img_transform)
+    print("Done.")
